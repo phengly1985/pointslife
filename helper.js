5139648dde39c7a0963c33343a735f419189751b
phengly1985 <76220211+phengly1985@users.noreply.github.com>
2020-12-18 09:13:41 -0700

    Add files via upload

---
+++ helper.js
@@ -0,0 +1,73 @@
+<!-- Central Javascript for Dynamic Content -->
+<script type="text/javascript">
+	document.write("testing javascript");
+	function amexHiltonAspireBonus()
+	{
+		document.write("<ul><li><b><a href='https://bit.ly/3h130J5' target='_blank'>Amex Hilton Aspire 150,000 Hilton Points</a></b></li></ul>");
+	}
+	
+	function amexHiltonEarn()
+	{
+		document.write("<li>With this card, you'll Earn:</li>");
+		  document.write("<ul>");
+			document.write("<li>");
+			  document.write("<b>14X Hilton Honors Bonus Points </b>for each dollar of eligible purchases charged on your Card directly with a hotel or resort within the Hilton portfolio.");
+			document.write("</li>");
+			document.write("<li>");
+			  document.write("<b>7X Hilton Honors Bonus Points</b> for each dollar of eligible purchases");
+			  document.write("on your Card on flights booked directly with airlines or amextravel.com,");
+			  document.write("and on car rentals booked directly from select car rental companies.");
+			document.write("</li>");
+			document.write("<li>");
+			  document.write("<b>7X Hilton Honors Bonus Points</b> for each dollar of eligible purchases");
+			  document.write("on your Card at U.S. restaurants.");
+			document.write("</li>");
+		  document.write("</ul>");
+	}
+	function amexBeforeApply()
+	{
+		document.write("<br/><h2>Before Applying - Amex Application Rules</h2>");
+		document.write("<div>");
+		  document.write("<ul>");
+			document.write("<li>");
+			  document.write("You can only get the welcome bonus on the card once in a lifetime with American Express, so be sure to apply when the welcome offer is high.");
+			document.write("</li>");
+			document.write("<li>");
+			  document.write("You can have at most 4 Amex credit cards. This includes personal and business credit cards.");
+			document.write("</li>");
+			document.write("<li>");
+			  document.write("Amex does not care about the number of hard credit inquiries on your credit report.");
+			document.write("</li>");
+			document.write("<li>");
+			  document.write("Multiple applications in the same day will only result in one hard credit inquiry on your credit report. However, you are limited to only 1 approved Amex credit card every 5-day period and 2 approved Amex credit cards every 90-day period.");
+			document.write("</li>")
+		  document.write("</ul>");
+		document.write("</div>");
+	}
+	
+	function amexAfterApply()
+	{
+		document.write("<br/><div><h2>After Applying</h2></div>");
+		document.write("<div>");
+			document.write("<ul>");
+			  document.write("<li>");
+				document.write("You can view your credit card's application status online <b><a href='https://online.americanexpress.com/eaol/welcome.do' target='_blank'>here</a></b>.");
+			  document.write("</li>");
+			  document.write("<li>");
+				document.write("If your application is under review or declined, you may contact Amex reconsideration line for personal &amp; business at ");
+				document.write("<b><a href='Tel:1-800-567-1083'>1-800-567-1083</a> </b>There's a chance they just need to verify you and your information in order to approve you.");
+			  document.write("</li>");
+  			  document.write("<li>If your application is instantly approved, congratulations.</li>");
+			  document.write("<li>Do not cancel, return purchases you made to meet the threshold amount or downgrade your card within 12 months after acquiring the welcome bonus offer. Amex may ban and close your account.");
+  			  document.write("</li>");
+			  document.write("<li>After the first year and when your next annual fee is posted to your account, you can decide whether to keep or close your card then. ");
+			  document.write("If you close your account, be sure to call Amex retention department at <b><a href='Tel:1-800-452-3945'>1-800-452-3945</a></b> to see if there are any offers available to you prior to closing.</li>");
+			  document.write("<li>You can also downgrade your card to the no annual fee Amex Hilton");
+  			  document.write("</li>")
+			document.write("</ul>");
+		  document.write("</div>");
+	}
+	
+	
+	
+</script>
