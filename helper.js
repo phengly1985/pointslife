945299f546c17451b2ad69d16721ffe357414569
pointslife <76220211+pointslife@users.noreply.github.com>
2020-12-18 09:45:11 -0700

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,8 +1,9 @@
 <!-- Central Javascript for Dynamic Content -->
 <script type="text/javascript">
-	alert("test");
-	
-	
+	function bonus()
+	{
+		document.write("<ul><li><b><a href='https://bit.ly/3h130J5' target='_blank'>Amex Hilton Aspire 150,000 Hilton Points</a></b></li></ul>");
+	}
 	
 	
 </script>
